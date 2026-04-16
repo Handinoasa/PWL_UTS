e151c801a053bdd8685e98dcfa4debaf8cb09254
Handinoasa <hasagalihr@gmail.com>
2026-04-16 10:16:20 +0700

    Create Migrations

--- database/migrations/2026_04_13_054315_create_m_users_table.php
+++ database/migrations/2026_04_13_054315_create_m_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('m_user', function (Blueprint $table) {
             $table->id('user_id');
-            $table->foreignId('level_id')->constrained('m_levels', 'level_id');
+            $table->foreignId('level_id')->constrained('m_level', 'level_id');
             $table->string('username', 20)->unique();
             $table->string('name', 100);
             $table->string('password', 255);
